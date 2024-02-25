73a8c581d81ed5a1ed6fa356856424f2f544bcfb
Egnoel <70013986+Egnoel@users.noreply.github.com>
2024-02-25 13:52:46 +0100

    backend intial setup

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+require('dotenv').config();
+
+// Connect to MongoDB
+mongoose
+  .connect(`${process.env.BD}/prices`)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// Define routes and middleware here
+app.use(cors());
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
