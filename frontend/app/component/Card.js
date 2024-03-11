9e5cd38b8768f297b37c1050000d9cfe6750ce3c
Egnoel <70013986+Egnoel@users.noreply.github.com>
2024-03-11 10:26:35 +0100

    Card & cardList

--- frontend/app/component/Card.js
+++ frontend/app/component/Card.js
@@ -1,9 +1,30 @@
+import Image from 'next/image';
 import React from 'react';
 
-const Card = () => {
+import { Button } from '@/components/ui/button';
+
+const Card = ({ name, price, image, weight }) => {
   return (
-    <div>
-      <p>Card</p>
+    <div className="flex  bg-white w-[300px] h-[130px] rounded-md">
+      <div className="w-1/2 h-full bg-red-300 rounded-md">
+        <Image
+          src={image}
+          alt="banana"
+          width={90}
+          height={90}
+          className="flex items-center justify-center object-cover w-full h-full rounded-md"
+        />
+      </div>
+      <div className="flex flex-col justify-center w-1/2 px-3">
+        <h1 className="text-lg font-bold">{name}</h1>
+        <p className="text-xs text-gray-300">{weight}</p>
+        <div className="flex items-center w-full gap-4 mt-2">
+          <p className="text-sm">Kz {price}</p>
+          <Button className="text-xs bg-green-400 text-slate-600 hover:bg-green-600">
+            Mais
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/app/component/CardList.js
+++ frontend/app/component/CardList.js
@@ -1,10 +1,60 @@
 import React from 'react';
 import Card from './Card';
+import banana from '../assets/yellow-banana-fruit.jpg';
+import pineapple from '../assets/pineapple.jpg';
+import apple from '../assets/apple.jpg';
+import orange from '../assets/orange.jpg';
+import mango from '../assets/mango.jpg';
 
 const CardList = () => {
+  const products = [
+    {
+      id: 1,
+      name: 'Banana',
+      price: 2.5,
+      image: banana,
+      weight: '1kg',
+    },
+    {
+      id: 2,
+      name: 'Apple',
+      price: 3.5,
+      image: apple,
+      weight: '1kg',
+    },
+    {
+      id: 3,
+      name: 'Orange',
+      price: 2.0,
+      image: orange,
+      weight: '1kg',
+    },
+    {
+      id: 4,
+      name: 'Mango',
+      price: 3.0,
+      image: mango,
+      weight: '1kg',
+    },
+    {
+      id: 5,
+      name: 'Pineapple',
+      price: 4.0,
+      image: pineapple,
+      weight: '1kg',
+    },
+  ];
   return (
-    <div className="flex flex-wrap w-full">
-      <Card />
+    <div className="flex flex-wrap w-full gap-4">
+      {products.map((product) => (
+        <Card
+          key={product.id}
+          name={product.name}
+          price={product.price}
+          image={product.image}
+          weight={product.weight}
+        />
+      ))}
     </div>
   );
 };
