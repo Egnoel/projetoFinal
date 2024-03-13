67cafd9edfc2b409e983a5487744089a2e0f69da
Egnoel <70013986+Egnoel@users.noreply.github.com>
2024-03-13 17:05:03 +0100

    AddProduct

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,16 +1,18 @@
 import Image from 'next/image';
 import Banner from './component/Banner';
 import CardList from './component/CardList';
+import { CirclePlus } from 'lucide-react';
+import AddProduct from './component/AddProduct';
 
 export default function Home() {
   return (
-    <div className="flex flex-row items-center gap-10">
+    <div className="flex flex-row gap-10">
       <div className="flex flex-col w-4/5 h-full gap-5">
         <Banner />
         <CardList />
       </div>
       <div className="flex flex-col w-1/5 h-full gap-10 ">
-        <p>Side</p>
+        <AddProduct />
       </div>
     </div>
   );
